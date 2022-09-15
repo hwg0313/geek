f2d01480847aeaa06dfaae7a1270f95652113afc
hwg <hwg0313@163.com>
2022-09-15 11:27:40 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// 导入路由
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import './App.css';
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 
+// 配置路由规则
 function App() {
   return (
-    <div className="App">
-      app1123123
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
