eb3408dd80c3c8d26303614b1fe0cc09b7bf0f76
hwg <hwg0313@163.com>
2022-10-14 16:40:53 +0800

    添加注释

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,16 +1,19 @@
 //封装ls存取token
 
 const key = "pc-key"
+//储存token
 const setToken = (token) => {
     return window.localStorage.setItem(key, token)
 }
-
+//获取token
 const getToken = () => {
     return window.localStorage.getItem(key)
 }
+//删除token
 const remiveToken = () => {
     return window.localStorage.removeItem(key)
 }
+//全部导出
 export {
     setToken, getToken, remiveToken
 }
